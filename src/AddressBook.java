dfd7c2872a6bf368324b01060b5e70a73e3b29b8
Hassan <hassan.jallad@hotmail.com>
2020-09-25 02:35:26 -0400

    added print statement in main

---
+++ src/AddressBook.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class AddressBook {
+    ArrayList<BuddyInfo> buddyList = new ArrayList<>();
+
+    public void addBuddy(BuddyInfo buddy){
+        buddyList.add(buddy);
+    }
+
+    public void removeBuddy(BuddyInfo buddy){
+        buddyList.add(buddy);
+    }
+}
